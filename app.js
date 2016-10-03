131f6107ccd73c7782ecf15fdfb26a3a11b2748d
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-10-03 15:25:19 -0700

    final clean up on JS objects

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ console.log('Connection Working!');
 var setsDisplayed = 0;
 var printSets = document.getElementById('products');
 var products = [];
+var randomNumberSets = [];
 
 function Products(productImages){
   this.productImages = productImages;
   this.numberOfTimesDisplayed = 0;
   this.numberOfTimesClicked = 0;
   this.imageLocation = 'img/' + productImages;
-  console.log('Products Array Working!');
+  this.productNumber = 0;
+  this.calcProductNumber();
   products.push(this);
 }
+Products.prototype.calcProductNumber = function () {
+  for (var i = 0; i < products.length; i++) {
+    this.productNumber = products.length;
+  }
+};
 
 new Products('bag.jpg');
 new Products('banana.jpg');
@@ -34,18 +41,23 @@ new Products('unicorn.jpg');
 new Products('usb.gif');
 new Products('water-can.jpg');
 new Products('wine-glass.jpg');
+console.log('Products Constructed!');
 
 function genRandomImage(max) {
   var ulEl = document.createElement('ul');
+  var randomNumbersGenerated = [];
   for (var i = 0; i < 3; i++) {
-    var randomNumber = Math.floor(Math.random() * (max - 0 + 1)) + 0;
+    var randomNumber = Math.floor(Math.random() * (max - 0)) + 0;
+    console.log('Random Product ' + (i + 1) + ': ' + randomNumber);
     var liEl = document.createElement('li');
     var imgEl = document.createElement('img');
     imgEl.src = products[randomNumber].imageLocation;
     liEl.appendChild(imgEl);
     ulEl.appendChild(liEl);
+    randomNumbersGenerated.push(randomNumber);
   }
   printSets.appendChild(ulEl);
-  console.log('Generate 3 random images working!');
+  randomNumberSets.push(randomNumbersGenerated);
+  console.log('3 random numbers pushed to array');
 }
 genRandomImage(products.length);
