201b986b453f50e34d032b250da4c6eb573c21d9
jonathanheemstra <jonathan.heemstra@gmail.com>
2016-10-03 13:51:00 -0700

    create random number using min 1 and max of product image array

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 'use strict';
 console.log('Connection Working!');
+
+var setsDisplayed = 0;
+var productImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg'];
+
+
+function genRandomImage(max) {
+  var randomNumber = Math.floor(Math.random() * (max - 1 + 1)) + 1);
+}
+console.log(productImages.length);
+
+for (var i = 0; i < productImages.length; i++) {
+  genRandomImage(productImages.length);
+}
